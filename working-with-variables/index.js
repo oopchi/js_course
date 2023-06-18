6ed3bbf140a1049be6039cd344318bc920263683
oopchi <calvinalfredo151@gmail.com>
2023-06-18 20:45:48 +0700

    feat: added answer for task `Working with variables`

---
+++ working-with-variables/index.js
@@ -0,0 +1,22 @@
+// @ts-nocheck
+"use strict";
+
+// Working with variables
+// importance: 2
+// Declare two variables: admin and name.
+// Assign the value "John" to name.
+// Copy the value from name to admin.
+// Show the value of admin using alert (must output “John”).
+
+
+// Declare two variables: admin and name.
+let admin, name;
+
+// Assign the value "John" to name.
+name = 'John';
+
+// Copy the value from name to admin.
+admin = name;
+
+// Show the value of admin using alert (must output “John”).
+alert(admin);
