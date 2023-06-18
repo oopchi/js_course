baed6795dffe1d8dd30e76e708c93ace5319ea7d
oopchi <calvinalfredo151@gmail.com>
2023-06-18 21:52:19 +0700

    refactor: added the question as comment in index.js for output even numbers in the loop

--- output-even-numbers-in-the-loop/index.js
+++ output-even-numbers-in-the-loop/index.js
@@ -1,6 +1,10 @@
 // @ts-nocheck
 "use strict";
 
+// Output even numbers in the loop
+// importance: 5
+// Use the for loop to output even numbers from 2 to 10.
+
 let n = 10;
 for (let i = 2; i <= n; i += 2) {
     alert(i);
